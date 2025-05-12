8dd96ba7bf4731e86c36aaa6748a57d72bd11bc7
Marko Medhat <85482481+Zer0bytte@users.noreply.github.com>
2025-05-12 10:11:01 +0300

    Update Coupon Code

---
+++ src/Electro/Ecommerce.API/Endpoints/CouponCode/UpdateCouponCodeEndpoint.cs
@@ -0,0 +1,40 @@
+﻿
+using Ecommerce.Application.Features.CouponCodes.Commands.UpdateCouponCode;
+using Ecommerce.Application.Features.CouponCodes.Queries.GetCouponCodes;
+
+namespace Ecommerce.API.Endpoints.CouponCode;
+
+public record UpdateCouponCodeRequest
+{
+    public string Code { get; set; } = default!;
+    public string? Description { get; set; }
+    public decimal DiscountPercentage { get; set; }
+    public decimal MaximumDiscountValue { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
+public class UpdateCouponCodeEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/api/coupons/{id}", async (UpdateCouponCodeRequest request, Guid id, ISender sender) =>
+       {
+           UpdateCouponCodeResult result = await sender.Send(new UpdateCouponCodeCommand
+
+           {
+               Id = id,
+               Code = request.Code,
+               Description = request.Description,
+               DiscountPercentage = request.DiscountPercentage,
+               ExpirationDate = request.ExpirationDate,
+               MaximumDiscountValue = request.MaximumDiscountValue,
+           });
+
+           return Results.Ok(ApiResponse<UpdateCouponCodeResult>.Success(result));
+
+       })
+               .RequireAuthorization("Admin")
+               .WithTags("Coupon Codes")
+               .WithSummary("Update Coupon Code")
+               .Produces<ApiResponse<UpdateCouponCodeResult>>();
+    }
+}
---
+++ src/Electro/Ecommerce.Application/Features/CouponCodes/Commands/UpdateCouponCode/UpdateCouponCodeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Features.CouponCodes.Commands.UpdateCouponCode;
+public class UpdateCouponCodeCommandValidator : AbstractValidator<UpdateCouponCodeCommand>
+{
+    public UpdateCouponCodeCommandValidator()
+    {
+        RuleFor(c => c.Code).NotEmpty().WithMessage("رمز الكوبون مطلوب!");
+        RuleFor(c => c.DiscountPercentage).GreaterThanOrEqualTo(1).WithMessage("يجب أن تكون نسبة الخصم 1% على الأقل");
+        RuleFor(c => c.MaximumDiscountValue).GreaterThanOrEqualTo(1).WithMessage("يجب أن تكون قيمة الخصم القصوى 1 على الأقل");
+        RuleFor(c => c.ExpirationDate).GreaterThan(DateTime.Now).WithMessage("يجب أن يكون تاريخ الانتهاء في المستقبل");
+    }
+}
