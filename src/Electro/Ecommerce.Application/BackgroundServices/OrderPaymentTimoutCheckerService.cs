86639d11f6f70a73f4e28d4255a61f1f77af39db
Marko Medhat <85482481+Zer0bytte@users.noreply.github.com>
2025-06-09 13:31:25 +0300

    Background check for unpaid orders

--- src/Electro/Ecommerce.Application/BackgroundServices/OrderPaymentTimoutCheckerService.cs
+++ src/Electro/Ecommerce.Application/BackgroundServices/OrderPaymentTimoutCheckerService.cs
@@ -11,7 +11,7 @@ public class OrderPaymentTimoutCheckerService : BackgroundService
 {
     private readonly IServiceScopeFactory scopeFactory;
     private readonly IBus bus;
-
+    private readonly TimeSpan _period = TimeSpan.FromSeconds(3);
     public OrderPaymentTimoutCheckerService(IServiceScopeFactory scopeFactory, IBus bus)
     {
         this.scopeFactory = scopeFactory;
@@ -19,7 +19,8 @@ public class OrderPaymentTimoutCheckerService : BackgroundService
     }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        using PeriodicTimer timer = new PeriodicTimer(_period);
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync())
         {
             using (var scope = scopeFactory.CreateScope())
             {
@@ -29,42 +30,39 @@ public class OrderPaymentTimoutCheckerService : BackgroundService
                     && o.PaymentStatus == PaymentStatus.Pending && o.Status != OrderStatus.Cancelled
                     && DateTime.UtcNow > o.OrderDate.AddMinutes(10)).ToListAsync(stoppingToken);
 
-                if (orders.Count == 0) {
-                    await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
-                    continue;
-                }
-
-                await context.Orders
-                    .Include(o => o.OrderItems)
-                    .Where(o => o.PaymentMethod == PaymentMethod.Online
-                    && o.PaymentStatus == PaymentStatus.Pending
-                    && DateTime.UtcNow > o.OrderDate.AddMinutes(10))
-                    .ExecuteUpdateAsync(prop => prop
-                    .SetProperty(p => p.Status, OrderStatus.Cancelled)
-                    .SetProperty(p => p.PaymentStatus, PaymentStatus.Failed)
-                    .SetProperty(p => p.CancellationReason, "Payment timeout")
-                    .SetProperty(p => p.Status, OrderStatus.Completed),
-                    stoppingToken);
-
-                await context.OrderItems
-                .Where(oi => oi.Order.PaymentMethod == PaymentMethod.Online
-                     && oi.Order.PaymentStatus == PaymentStatus.Pending
-                     && DateTime.UtcNow > oi.Order.OrderDate.AddMinutes(10))
-                .ExecuteUpdateAsync(prop => prop
-                .SetProperty(p => p.Status, OrderItemStatus.Cancelled)
-                .SetProperty(p => p.CancellationReason, "Payment timeout"), stoppingToken);
-
-
-                foreach (var order in orders)
+                if (orders.Count > 0)
                 {
-                    await bus.Publish(new OrderCancelledEvent
-                    {
-                        OrderId = order.Id
-                    }, stoppingToken);
-                }
-                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
-            }
+                    await context.Orders
+                        .Include(o => o.OrderItems)
+                        .Where(o => o.PaymentMethod == PaymentMethod.Online
+                        && o.PaymentStatus == PaymentStatus.Pending
+                        && DateTime.UtcNow > o.OrderDate.AddMinutes(10))
+                        .ExecuteUpdateAsync(prop => prop
+                        .SetProperty(p => p.Status, OrderStatus.Cancelled)
+                        .SetProperty(p => p.PaymentStatus, PaymentStatus.Failed)
+                        .SetProperty(p => p.CancellationReason, "Payment timeout")
+                        .SetProperty(p => p.Status, OrderStatus.Completed),
+                        stoppingToken);
 
+                    await context.OrderItems
+                    .Where(oi => oi.Order.PaymentMethod == PaymentMethod.Online
+                         && oi.Order.PaymentStatus == PaymentStatus.Pending
+                         && DateTime.UtcNow > oi.Order.OrderDate.AddMinutes(10))
+                    .ExecuteUpdateAsync(prop => prop
+                    .SetProperty(p => p.Status, OrderItemStatus.Cancelled)
+                    .SetProperty(p => p.CancellationReason, "Payment timeout"), stoppingToken);
+
+
+                    foreach (var order in orders)
+                    {
+                        await bus.Publish(new OrderCancelledEvent
+                        {
+                            OrderId = order.Id
+                        }, stoppingToken);
+                    }
+
+                }
+            }
         }
     }
 }
